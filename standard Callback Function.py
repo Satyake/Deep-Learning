ef185a28b94655332fae6100a465747768679e43
Satyake <35741572+Satyake@users.noreply.github.com>
2021-05-24 20:32:44 -0400

    Update standard Callback Function.py

--- standard Callback Function.py
+++ standard Callback Function.py
@@ -1,5 +1,7 @@
  class callbacks(tf.keras.callbacks.Callback):
-        def epoch_end(self,epoch,logs={}):
-            if (logs.get('accuracy')>0.99):
+        def on_epoch_end(self,epoch,logs={}):
+            if (logs.get('acc')>0.99):
                 print('Reached desired accuracy cancelling Training!')
                 self.model.stop_training=True
+
+    callback=callbacks()
